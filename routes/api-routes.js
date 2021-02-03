6a0df6a10d2801d4187d119783fcb9f9c35897fd
Garrett Steed <steed.garrett@gmail.com>
2021-02-03 15:05:02 -0700

    built out put and delete in api routes, still not working but getting message returns now

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,12 +12,35 @@ app.post("/api/workouts", (req, res) => {
 });
 
 app.get("/api/workouts", (req, res) => {
-    db.Fitness.find({}, (err, fitnessDB) => {
+    Fitness.find({}, (err, fitnessDB) => {
         if(err){
             console.log(err);
         } else {
             res.json(fitnessDB)
         }
+    });
+});
+
+app.put("/api/workouts/:id", ({ body, params}, res) => {
+    Fitness.findByIdAndUpdate(
+        params.id,
+        { $push: { exercise: body } } ,
+    )
+    .then(fitnessDB => {
+        res.json(fitnessDB);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+app.delete("/api/workouts", ({ body }, res) => {
+  Fitness.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
     });
 });
     
